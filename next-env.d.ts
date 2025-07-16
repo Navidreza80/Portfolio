ec0283130e4326ab1542bd27cdbae09aadfe4382
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-07-16 12:24:05 +0330

    Add next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+declare module '*.svg' {
+  import * as React from 'react';
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
