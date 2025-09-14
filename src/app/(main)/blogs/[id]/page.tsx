8b27da52774d8e8cd14186eeffd58212841f5ea0
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-09-14 17:35:53 +0330

    Fix type check

--- src/app/(main)/blogs/[id]/page.tsx
+++ src/app/(main)/blogs/[id]/page.tsx
@@ -8,7 +8,11 @@ import { format } from "date-fns";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-export default async function PostPage({ params }: { params: { id: string } }) {
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const post = await getPostById(id);
   const userId = await getServerCookie("userId");
